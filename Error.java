9d5e207e9679fbb46bd356043b042ce9e972f2bf
Wuttha <Wuttha@webmail.npru.ac.th>
2025-06-27 16:04:14 +0700

    first commit

---
+++ Error.java
@@ -0,0 +1,12 @@
+public class Error {
+    public static void main(String args[]) {
+        // 1. Compile/Syntax Error
+        //System.out.println("Hello ");
+
+        // 2. Runtime Error
+        //System.out.println(5/0);
+
+        // 3.Logical Error
+        System.out.println(4.0/10);
+    }
+}
